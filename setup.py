90a8434281144aaaa09ac42a652dce639c010517
Casper Smet <casper.smet@student.hu.nl>
2020-04-22 11:41:51 +0200

    Set up for pip install

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Lazytest',
+    version='1.0',
+    description='I was tired of making new .py files for each test I need to write, so I wrote a simple script to do it for me.',
+    author='Casper Smet',
+    author_email='casper.smet@gmail.com',
+    packages=find_packages()
+)
