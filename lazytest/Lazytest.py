3065fe5e2b2183ea6b49b8dad6a6aca90e608cc8
Casper Smet <casper.smet@student.hu.nl>
2020-04-22 19:01:23 +0200

    Lazytester can now make tests for class methods

--- lazytest/Lazytest.py
+++ lazytest/Lazytest.py
@@ -2,47 +2,90 @@ from sys import argv
 import os
 
 
-def main():
-    """Makes a test format file for python scripts"""
-    assert len(argv) > 1, "ERROR: Missing file path"
-    fp = argv[1]
-    assert os.path.isfile(fp) and fp[-3:] == ".py", f"ERROR: No .py file found at {fp}"
+def split_class_def(iter_):
+    """Splits into class or def lines."""
+    iter_ = iter_.strip()
+    if iter_.startswith("def"):
+        iter_ = iter_[3:]
+    # elif iter_.startswith("class"):
+    #     x = iter_[6:-2]
 
-    if len(argv) > 2:
-        out = argv[2]
-        assert os.path.isdir(out), f"ERROR: No (empty) directory found at {out}"
-    else:
-        out = os.path.dirname(fp)
+    return iter_
 
-    file_in_name = os.path.basename(fp)
-    filename = "test_" + file_in_name
-    output = os.path.join(out, filename)
+class LazyTester:
+    def __init__(self, fp, out=None):
 
-    out_file_exists = os.path.exists(output)
+        assert os.path.isfile(fp) and fp[-3:] == ".py", f"ERROR: No .py file found at {fp}"
+        self.fp = fp
+        if out:
+            assert os.path.isdir(out), f"ERROR: No (empty) directory found at {out}"
+            self.out = out
+        else:
+            self.out = os.path.dirname(fp)
 
-    with open(fp, "r") as infile:
-        function_lines = filter(lambda x: x.startswith("def"), infile.readlines())
-        extracted_names = map(lambda x: x[3:].split("(")[0].strip(), function_lines)
-        if out_file_exists:
-            with open(output, "r") as outread:
-                test_lines = filter(lambda x: x.startswith("def"), outread.readlines())
-                extracted_tests = map(lambda x: x[9:].split("(")[0].strip(), test_lines)
-                extracted_names = filter(lambda x: x not in extracted_tests, extracted_names)
+        self.file_in_name = os.path.basename(fp)
+        self.filename = "test_" + self.file_in_name
+        self.output = os.path.join(self.out, self.filename)
 
-        with open(output, "a+") as outfile:
-            if not out_file_exists:
-                if len(argv) > 2:
-                    outfile.write("import os\n")
-                    outfile.write(f"os.path.append(r\"{os.path.dirname(fp)}\")\n")
-                outfile.write(f"import {file_in_name[:-3]}\n")
-                outfile.write("import pytest")
+        self.out_file_exists = os.path.exists(self.output)
 
-            for func in extracted_names:
+    def makeLazy(self):
+        """Makes a lazytest file."""
+        with open(self.fp, "r") as infile:
+            function_lines = filter(lambda x: x.strip().startswith(("def ", "class ")), infile.readlines())
+            extracted_names = map(split_class_def, function_lines)
+            if self.out_file_exists:
+                extracted_names = self.filter_functions(extracted_names)
+
+            with open(self.output, "a+") as outfile:
+                if not self.out_file_exists:
+                    self.header(outfile)
+                self.write_functions(outfile, extracted_names)
+
+                outfile.write("\n")
+
+    def filter_functions(self, funcs):
+        """Filters functions that are already in test file."""
+        with open(self.output, "r") as outread:
+            test_lines = filter(lambda x: x.startswith("def"), outread.readlines())
+            extracted_tests = map(lambda x: x[9:].split("(")[0].strip(), test_lines)
+            extracted_names = filter(lambda x: x not in extracted_tests, funcs)
+        return extracted_names
+
+    def header(self, outfile):
+        """Header lines."""
+        if len(argv) > 2:
+            outfile.write("import os\n")
+            outfile.write(f"os.path.append(r\"{os.path.dirname(self.fp)}\")\n")
+        outfile.write(f"import {self.file_in_name[:-3]}\n")
+        outfile.write("import pytest")
+
+    def write_functions(self, outfile, funcs):
+        """Writes functions to outfile."""
+        classname = None
+        for func in funcs:
+            if func.startswith("class "):
+                classname = func[6:-1].strip()
+            else:
+                class_method = "self" in func
+                func = func.split("(")[0].strip()
+                if class_method:
+                    func = f"{classname}_{func}"
                 outfile.write(f"\n\n\ndef test_{func}():\n")
-                outfile.write(f"    \"\"\"Tests {file_in_name}.{func}.\"\"\"\n")
+                outfile.write(f"    \"\"\"Tests {self.file_in_name}.{func}.\"\"\"\n")
                 outfile.write("    assert False")
 
-            outfile.write("\n")
+
+def main():
+    """Makes a test format file for python scripts."""
+    assert len(argv) > 1, "ERROR: Missing file path"
+    fp = argv[1]
+    if len(argv) > 2:
+        out = argv[2]
+    else:
+        out = None
+        
+    LazyTester(fp, out=out).makeLazy()
 
 
 if __name__ == "__main__":
